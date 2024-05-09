d6de0777daf4d97a7e9d7fa20c1ea52c3328dbe3
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-05-09 20:39:02 +0900

    메인뷰 숫자수정

--- src/camp/view/MainView.java
+++ src/camp/view/MainView.java
@@ -22,7 +22,7 @@ public class MainView {
         System.out.println("내일배움캠프 수강생 관리 프로그램 실행 중...");
         System.out.println("1. 수강생 관리");
         System.out.println("2. 점수 관리");
-        System.out.println("4. 프로그램 종료");
+        System.out.println("3. 프로그램 종료");
         System.out.print("관리 항목을 선택하세요...");
         return UtilHelper.getIntInput(scanner);
     }
